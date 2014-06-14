0702a5f1274d415942e0f9eaf2cca3f4aec97eb9
Eric Kelly <heroiceric@gmail.com>
2014-06-14 11:51:23 -0400

    Setup frontend with ember

---
+++ frontend/app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: RocketJumpENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ frontend/tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'rocket-jump'
+};
+
+export default resolver;
