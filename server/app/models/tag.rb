1cb28b7db4bd23e27185c8a3cc27b8e944d843da
Richard Davis <rdavis.bacs@gmail.com>
2014-06-14 12:44:25 -0400

    taggable module

--- server/app/models/tag.rb
+++ server/app/models/tag.rb
@@ -2,7 +2,6 @@ class Tag < ActiveRecord::Base
   has_many :taggings
 
   has_many :questions, through: :taggings, source: :taggable, source_type: Question
-  has_many :replies, through: :taggings, source: :taggable, source_type: Reply
   has_many :hyperlinks, through: :taggings, source: :taggable, source_type: Hyperlink
 
   validates_presence_of :name
