db37bb3b3a966995a67ff092ae6017e929848a66
Richard Davis <rdavis.bacs@gmail.com>
2014-06-14 11:56:34 -0400

    add validations

--- server/app/models/question.rb
+++ server/app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ActiveRecord::Base
   has_many :replies
+
+  validates_presence_of :body
 end
--- server/app/models/reply.rb
+++ server/app/models/reply.rb
@@ -1,3 +1,5 @@
 class Reply < ActiveRecord::Base
   belongs_to :question
+
+  validates_presence_of :body
 end
