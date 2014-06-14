71f4cebf179bfd52cc589d315b3c5e2d7a59d71b
Eric Kelly <heroiceric@gmail.com>
2014-06-14 14:39:04 -0400

    Add title to questions

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140614175916) do
+ActiveRecord::Schema.define(version: 20140614182838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140614175916) do
     t.text     "body",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title",      null: false
   end
 
   create_table "replies", force: true do |t|
